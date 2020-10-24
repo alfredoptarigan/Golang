8b56c742ce785ca4891cf16182c7488824b89c29
Alfredo Patricius Tarigan <62396803+alfredoptarigan@users.noreply.github.com>
2020-10-24 15:31:05 +0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Alfredo P Tarigan"
+	var healthPoint int
+	age := 20 // membuat variabel tanpa harus mmemuat tipe data
+	fmt.Println(name)
+	fmt.Println(healthPoint)
+	fmt.Println(age)
+}
